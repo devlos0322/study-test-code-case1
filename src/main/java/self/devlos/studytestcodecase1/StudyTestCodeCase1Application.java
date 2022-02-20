6b9b69ae5a3b13beeda606e0013fbd2750c5efd0
devlos0322 <devlos0322@gmail.com>
2022-02-20 16:45:38 +0900

    Initial commit

---
+++ src/main/java/self/devlos/studytestcodecase1/StudyTestCodeCase1Application.java
@@ -0,0 +1,13 @@
+package self.devlos.studytestcodecase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTestCodeCase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTestCodeCase1Application.class, args);
+	}
+
+}
---
+++ src/test/java/self/devlos/studytestcodecase1/StudyTestCodeCase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package self.devlos.studytestcodecase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTestCodeCase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
